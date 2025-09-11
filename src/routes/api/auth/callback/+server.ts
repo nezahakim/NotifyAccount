49a38816ac1a59a276e63c74ea123e5de1008441
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-09-11 17:33:47 +0200

    v.0.1

--- src/routes/api/auth/callback/+server.ts
+++ src/routes/api/auth/callback/+server.ts
@@ -9,7 +9,7 @@ export const  GET: RequestHandler = async ({ url, cookies }) => {
 		if (!token) {
 			throw redirect(302, '/login?error=missing_token');
 		}
-        
+
 		const res = await fetch(`${AUTH_SERVER}/api/auth/validate`, {
 			headers: {
 				Authorization: `Bearer ${token}`
@@ -21,22 +21,6 @@ export const  GET: RequestHandler = async ({ url, cookies }) => {
 		}
 
 		const data = await res.json();
-		
-        cookies.set('auth_token', data.user.sessionId, {
-            path: '/',
-            httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            maxAge: 60 * 60 * 24 * 7 // 7 days
-        });
-
-        cookies.set('auth_token_1', data.user.id, {
-            path: '/',
-            httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            maxAge: 60 * 60 * 24 * 7 // 7 days
-        });
 
 		return json({
 			user: data?.user,
