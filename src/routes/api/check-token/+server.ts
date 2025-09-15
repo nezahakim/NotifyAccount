8427979956ccb868ff214bbc4d4ea0ff82fe0f2d
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-09-15 11:13:12 +0200

    v.0.1

--- src/routes/api/check-token/+server.ts
+++ src/routes/api/check-token/+server.ts
@@ -1,7 +1,7 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ cookies }) => {
-    const refreshToken = cookies.get('refresh_token');
+    const refreshToken = cookies.get('auth_token');
 
     if (!refreshToken) {
         return json({ success: false, message: 'Unauthorized - attempt detected' }, { status: 401 });
