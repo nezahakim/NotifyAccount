1743a76caff3e93a85c9e304dc357b32fc6c5699
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-10-14 12:09:06 +0200

    fix some

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { authStore } from '$lib/stores/auth';
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
 
 	let { children } = $props();
 </script>
