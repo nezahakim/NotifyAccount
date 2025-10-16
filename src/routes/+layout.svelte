6e253fe3306ea1f95b7eb4cd87fd16993c9e87bd
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-10-16 18:05:10 +0200

    add fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { authStore } from '$lib/stores/auth';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	let user_email = $state<string>('')
+	let is_email_set = $state<boolean>(false)
+
+	onMount(()=>{
+		if($authStore.user && $authStore.user.email){
+			is_email_set = true;
+			user_email = $authStore.user.email;
+		}
+		
+	})	
+
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>`${is_email_set ? 'NotifyAccount | ('+user_email+')' : 'NotifyAccount'}`</title>
 </svelte:head>
 
 {@render children?.()}
